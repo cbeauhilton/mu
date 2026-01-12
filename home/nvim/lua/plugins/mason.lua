1b164892916fe5f500e1a2dd89cfdb5a2d7a03de
Beau Hilton <beau@beauhilton.com>
2026-01-12 14:53:25 -0600

    remove old nvim setup

--- home/nvim/lua/plugins/mason.lua
+++
@@ -1,6 +0,0 @@
-return {
-  -- Mason is disabled - using NixOS-native LSP servers instead
-  -- pyrefly is installed via NixOS packages (see home.nix)
-  { "mason-org/mason-lspconfig.nvim", enabled = false },
-  { "mason-org/mason.nvim", enabled = false },
-}
