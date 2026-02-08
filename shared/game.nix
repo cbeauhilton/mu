4696f65d3af6d8ec65608cba9d7c7000542c5684
Beau Hilton <beau@beauhilton.com>
2026-02-07 21:09:54 -0600

    refactor(gaming): gate behind custom.gaming.enable option

--- shared/game.nix
+++ shared/game.nix
@@ -1,29 +1,38 @@
-{pkgs, ...}: {
-  programs = {
-    steam.enable = true;
-    gamescope.enable = true;
-    gamemode = {
-      enable = true;
-      settings.general.inhibit_screensaver = 0;
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}: {
+  options.custom.gaming.enable = lib.mkEnableOption "gaming packages and configuration";
+
+  config = lib.mkIf config.custom.gaming.enable {
+    programs = {
+      steam.enable = true;
+      gamescope.enable = true;
+      gamemode = {
+        enable = true;
+        settings.general.inhibit_screensaver = 0;
+      };
     };
-  };
 
-  environment.systemPackages = with pkgs; [
-    (heroic.override {
-      extraPkgs = pkgs: [
-        pkgs.gamemode
-        pkgs.gamescope
-        pkgs.steam-run
-      ];
-    })
-  ];
+    environment.systemPackages = with pkgs; [
+      (heroic.override {
+        extraPkgs = pkgs: [
+          pkgs.gamemode
+          pkgs.gamescope
+          pkgs.steam-run
+        ];
+      })
+    ];
 
-  users.users.beau.extraGroups = ["gamemode"];
+    users.users.beau.extraGroups = ["gamemode"];
 
-  # Roblox flatpak repo
-  systemd.services.flatpak-repo = {
-    wantedBy = ["multi-user.target"];
-    path = [pkgs.flatpak];
-    script = "flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo";
+    # Roblox flatpak repo
+    systemd.services.flatpak-repo = {
+      wantedBy = ["multi-user.target"];
+      path = [pkgs.flatpak];
+      script = "flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo";
+    };
   };
 }
